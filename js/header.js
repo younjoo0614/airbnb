11db24cd529ba67c2718a7a183417ef6bbdc95d0
kjw7953 <kjw7953@github.com>
2020-05-02 10:14:48 +0900

    add jQuery function

--- js/header.js
+++ js/header.js
@@ -8,15 +8,18 @@ $(document).ready(function(){
         var barThis = $("#topBar")
         var fixNext = $("#fixNextContent")
         var navDiv = $("#nav-banner")
+        var afchang = $("#after-change")
 
         if( docScrollY > topBar.top ) {
             barThis.addClass("top-bar-fix");
-            navDiv.addClass("nav-none")
+            navDiv.addClass("nav-none");
             fixNext.addClass("pd-top-80");
+            afchang.addClass("nav-change");
         }else{
             barThis.removeClass("top-bar-fix");
             fixNext.removeClass("pd-top-80");
-            navDiv.removeClass("nav-none")
+            navDiv.removeClass("nav-none");
+            afchang.removeClass("nav-change");
         }
 
     });
